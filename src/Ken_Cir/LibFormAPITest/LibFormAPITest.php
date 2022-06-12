68dd78953b5304a72d19dbb5d5b7f3f90a25e757
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-06-12 09:26:53 +0900

    a

--- src/Ken_Cir/LibFormAPITest/LibFormAPITest.php
+++ src/Ken_Cir/LibFormAPITest/LibFormAPITest.php
@@ -14,6 +14,8 @@ use Ken_Cir\LibFormAPI\Forms\CustomForm;
 use Ken_Cir\LibFormAPI\Forms\ModalForm;
 use Ken_Cir\LibFormAPI\Forms\SimpleForm;
 use Ken_Cir\LibFormAPI\FormContents\SimpleForm\SimpleFormButton;
+use Ken_Cir\LibFormAPI\FormStack\StackFormManager;
+use Ken_Cir\LibFormAPI\Utils\Util;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
@@ -22,6 +24,13 @@ use pocketmine\utils\TextFormat;
 
 class LibFormAPITest extends PluginBase
 {
+    private StackFormManager $stackFormManager;
+
+    protected function onEnable(): void
+    {
+        $this->stackFormManager = new StackFormManager();
+    }
+
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
         if (!$sender instanceof Player) {
@@ -42,7 +51,8 @@ class LibFormAPITest extends PluginBase
                 function (Player $player) {
                     $player->sendMessage("フォームが閉じられました");
                 });
-        } elseif ($command->getName() === "modalform") {
+        }
+        elseif ($command->getName() === "modalform") {
             new ModalForm($sender,
                 "テスト ModalForm",
                 "テスト",
@@ -54,7 +64,8 @@ class LibFormAPITest extends PluginBase
                 function (Player $player) {
                     $player->sendMessage("フォームが閉じられました");
                 });
-        } elseif ($command->getName() === "customform") {
+        }
+        elseif ($command->getName() === "customform") {
             new CustomForm($sender,
                 "テスト CustomForm",
                 [
@@ -83,7 +94,57 @@ class LibFormAPITest extends PluginBase
                 function (Player $player) {
                     $player->sendMessage("フォームが閉じられました");
                 });
-        } else return false;
+        }
+        elseif ($command->getName() === "backform") {
+            $form = new CustomForm($sender,
+                "テスト CustomForm",
+                [
+                    // 必須パラメーターでのテスト
+                    new ContentLabel("テスト Label"),
+                    new ContentToggle("テスト Toggle"),
+                    new ContentSlider("テスト Slider", 0, 10),
+                    new ContentStepSlider("テスト StepSlider", ["1", "5", "10"]),
+                    new ContentDropdown("テスト Dropdown", ["0", "1", "2"]),
+                    new ContentInput("テスト Input"),
+
+                    // オプションパラメーターを使用してのテスト
+                    new ContentLabel("テスト Label"),
+                    new ContentToggle("テスト Toggle", true),
+                    new ContentSlider("テスト Slider", 1, 10, 5),
+                    new ContentSlider("テスト Slider", 1, 10, 5, 3),
+                    new ContentStepSlider("テスト StepSlider", ["1", "5", "10"], 1),
+                    new ContentDropdown("テスト Dropdown", ["0", "1", "2"], 2),
+                    new ContentInput("テスト Input", "テスト"),
+                    new ContentInput("テスト Input", "テスト", "TEST"),
+                ],
+                function (Player $player, array $data) {
+                    new ModalForm($player,
+                        "BackForm機能のテスト",
+                        "BackForm機能のテスト",
+                        "戻る",
+                        "そのまま閉じる",
+                        function (Player $player, bool $data) {
+                            if ($data) {
+                                $player->sendMessage("3秒後前のFormに戻ります");
+                                Util::backForm($this, [$this->stackFormManager->getStackFormEnd($player->getXuid()), "reSend"], [], 3);
+                            }
+                            else {
+                                $player->sendMessage("処理を終わります");
+                                $this->stackFormManager->deleteStack($player->getXuid());
+                            }
+                        },
+                        function (Player $player) {
+                            $player->sendMessage("フォームが閉じられました");
+                            $this->stackFormManager->deleteStack($player->getXuid());
+                        });
+                },
+                function (Player $player) {
+                    $player->sendMessage("フォームが閉じられました");
+                });
+
+            $this->stackFormManager->addStackForm($sender->getXuid(), "test_modal", $form);
+        }
+        else return false;
 
         return true;
     }
